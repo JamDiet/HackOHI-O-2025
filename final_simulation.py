fb86fa429be37ccaad4d2439e370ad14142e0fdb
JamDiet <eschbachjonathan1@gmail.com>
2025-10-25 23:24:16 -0400

    Weights fixed

--- final_simulation.py
+++ final_simulation.py
@@ -4,7 +4,7 @@ import numpy as np
 # Assemble boarding order from optimized sequences
 boarding_order = []
 for i in range(3):
-    weights = np.load(f'data\\boarding_orders\\boarding_group_{i}')
+    weights = np.load(f'data\\boarding_orders\\boarding_group_{i}.npy')
     boarding_order.append(order_by_weights(np.arange(i*10+1, i*10+11), weights))
 boarding_order = np.concatenate(tuple(boarding_order))
 
